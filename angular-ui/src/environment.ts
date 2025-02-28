4712c94dab6bf0010d84dec7f1736d81c1a154e2
Logeshwaran-N <logeshwaran082@gmail.com>
2025-02-28 22:19:57 +0530

    cleaned up

--- angular-ui/src/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    api_endpoint: '/api',
-    grafana_endpoint: '/grafana'
-};
--- deployment/bootup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl apply -f sender.yml
-kubectl apply -f trader.yml
-# kubectl apply -f peak-generator.yml
-kubectl apply -f middleware.yml
-kubectl apply -f angular.yml
